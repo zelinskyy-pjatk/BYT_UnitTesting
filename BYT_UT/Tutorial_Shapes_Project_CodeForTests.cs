6bf1b4352cf7f5bdfc963018d58a020b713bae98
Dmytro Zelinskyy <dmytrozelinskyy07@gmail.com>
2025-11-04 01:14:32 +0100

    Sphere Test Added Commit

--- BYT_UT/Tutorial_Shapes_Project_CodeForTests.cs
+++ BYT_UT/Tutorial_Shapes_Project_CodeForTests.cs
@@ -2,6 +2,26 @@ namespace BYT_UT
 {
     public class Tests
     {
-        
+        private IShape sphere = new Sphere(5);
+
+        // -- Sphere Tests -- //
+        [Test]
+        public void TestSphereCalculateArea()
+        {
+            Assert.That(sphere.CalculateArea(), Is.EqualTo(314.159).Within(0.001));
+        }
+
+        [Test]
+        public void TestSphereCalculateVolume()
+        {
+            Assert.That(sphere.CalculateVolume(), Is.EqualTo(523.598).Within(0.001));
+        }
+
+        [Test]
+        public void TestSphereNegativeRadius_PositiveArea()
+        {
+            sphere = new Sphere(-5);
+            Assert.That(sphere.CalculateArea(), Is.Positive);
+        }
     }
 }
